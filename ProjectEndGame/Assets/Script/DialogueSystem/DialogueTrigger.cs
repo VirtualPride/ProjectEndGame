51385865a5eec9a8e9995bf794939fca38158f9b
OnealTristan <tristanjulian37@gmail.com>
2023-10-29 15:56:42 +0700

    Tristan-Update-Dialogue
    Update dialogue 2 player kanan/kiri

--- ProjectEndGame/Assets/Script/DialogueSystem/DialogueTrigger.cs
+++ ProjectEndGame/Assets/Script/DialogueSystem/DialogueTrigger.cs
@@ -2,14 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour
+public class DialogueTrigger : MonoBehaviour, IInteractable
 {
 
     public Dialogue dialogue;
+	private string interactText = "Talk";
 
-    public void TriggerDialogue()
+	public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
+
+	public string GetInteractText()
+	{
+		return interactText;
+	}
+
+	public Transform GetTransform()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void Interact()
+	{
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+			FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+		}
+		else if (Input.GetKeyDown(KeyCode.M))
+		{
+			FindObjectOfType<DialogueManagerPlayer2>().StartDialogue(dialogue);
+		}
+	}
 
 }
