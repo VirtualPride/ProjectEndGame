dd1e641cac86489dba9c5e6cf40d3ce57426353f
OnealTristan <tristanjulian37@gmail.com>
2023-11-30 21:25:45 +0700

    Tristan-Update-Dialogue
    Implement dialogue system in game

--- ProjectEndGame/Assets/Script/DialogueSystem2/DialogueManage2.cs
+++ ProjectEndGame/Assets/Script/DialogueSystem2/DialogueManage2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class DialogueManage : MonoBehaviour
+public class DialogueManage2 : MonoBehaviour
 {
     public Image actorImage;
     public TextMeshProUGUI actorName;
